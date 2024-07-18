016dede5426c1d1340582d6b8ac5bd2f04d7d29c
heetbeet <4103775+heetbeet@users.noreply.github.com>
2024-07-18 15:23:08 +0200

    Keep local ini file for portable texstudio

--- bin/TeXstudio.cmd
+++ bin/TeXstudio.cmd
@@ -1,3 +1,12 @@
 @echo off
+setlocal
 set "PATH=%~dp0..\programs\miktex-portable\texmfs\install\miktex\bin\x64;%PATH%"
-call "%~dp0..\programs\texstudio-3.0.1-win-portable-qt5\texstudio.exe" %*
+set "ini_file=%~dp0..\programs\texstudio-4.8.1-win-portable-qt6\texstudio.ini"
+set "exe_file=%~dp0..\programs\texstudio-4.8.1-win-portable-qt6\texstudio.exe"
+
+if not exist "%ini_file%" (
+    type nul > "%ini_file%"
+)
+
+call "%exe_file%" %*
+endlocal
